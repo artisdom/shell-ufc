ca264569b1e2865ee0a1bfd3ad4dd45ec6edf336
2legal2 <joel.wejdenstal@gmail.com>
2018-04-30 10:58:05 +0000

    Fixed some compat errors

--- shell-ufc/functions/git/git.s
+++ shell-ufc/functions/git/git.s
@@ -1,15 +1,19 @@
 git() {
-    if [ "$1" == "gca" ]
+    if [ "$1" = "gca" ]
     then
         git gc --aggressive
-    elif [ "$1" == "sync" ]
+    elif [ "$1" = "sync" ]
     then
         git pull
         git push
-    elif [ "$1" == "ac" ]
+    elif [ "$1" = "ac" ]
     then
         git add .
         git commit $2
+    elif [ "$1" = "caa" ]
+    then
+        git add .
+        git commit --amend -C HEAD
     else
         command git $@
     fi
