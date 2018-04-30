e8202a1b9a955b1f1248115b0bbd764dc5148af9
2legal2 <joel.wejdenstal@gmail.com>
2018-04-30 12:09:08 +0000

    Made the git code module a bit more readable

--- shell-ufc/functions/git/git.sh
+++ shell-ufc/functions/git/git.sh
@@ -2,21 +2,26 @@ git() {
     if [ "$1" = "gca" ]
     then
         git gc --aggressive
+
     elif [ "$1" = "sync" ]
     then
         git pull
         git push
+
     elif [ "$1" = "ac" ]
     then
         git add .
         git commit -m $2
+
     elif [ "$1" = "caa" ]
     then
         git add .
         git commit --amend -C HEAD
+
     elif [ "$1" = "ls" ]
     then
         git log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate
+
     else
         command git $@
     fi
