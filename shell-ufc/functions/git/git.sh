16cc38fc1dd265a01044f5ac62ad242af63453ce
2legal2 <joel.wejdenstal@gmail.com>
2018-04-30 12:11:04 +0000

    Added a command to change the last commit message

--- shell-ufc/functions/git/git.sh
+++ shell-ufc/functions/git/git.sh
@@ -22,6 +22,10 @@ git() {
     then
         git log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate
 
+    elif [ "$1" = "acn" ]
+    then
+        git commit --amend
+
     else
         command git $@
     fi
